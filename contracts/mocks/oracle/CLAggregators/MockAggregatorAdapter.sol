213e8a6bd3ee6c0e3a480aca3a4512cc8432df5b
atatur9 <momodarou2015@gmail.com>
2022-02-10 22:02:55 +0900

    update address

---
+++ contracts/mocks/oracle/CLAggregators/MockAggregatorAdapter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.6.12;
+import {IPriceAggregatorAdapter} from '../../../interfaces/IPriceAggregatorAdapter.sol';
+
+contract MockAggregatorAdapter is IPriceAggregatorAdapter {
+  mapping(address => int256) private _prices;
+
+  event AnswerUpdated(address[] assets, int256[] indexed prices);
+
+  constructor(address[] memory assets, int256[] memory prices) public {
+    require(assets.length == prices.length);
+    for (uint256 i = 0; i < assets.length; i++) {
+      _prices[assets[i]] = 1;
+    }
+  }
+
+  function currentPrice(address asset) external view override returns (int256) {
+    return _prices[asset];
+  }
+}
