64c2bc77647fd22a2ff2d0d888fee3c6630724ca
atatur9 <momodarou2015@gmail.com>
2022-02-28 03:04:12 +0900

    update scripts

--- tasks/helpers/liquidation-test.ts
+++ tasks/helpers/liquidation-test.ts
@@ -1,3 +1,4 @@
+import { waitForTx } from './../../helpers/misc-utils';
 import { ERC20Factory } from '../../types/ERC20Factory';
 import BigNumber from 'bignumber.js';
 import { parseEther } from 'ethers/lib/utils';
@@ -12,7 +13,11 @@ import {
 import { getEthersSigners } from '../../helpers/contracts-helpers';
 import { eNetwork, ICommonConfiguration } from '../../helpers/types';
 import { getFirstSigner } from '../../helpers/contracts-getters';
-import { LendingPoolFactory, StarlayProtocolDataProviderFactory } from '../../types';
+import {
+  LendingPoolCollateralManagerFactory,
+  LendingPoolFactory,
+  StarlayProtocolDataProviderFactory,
+} from '../../types';
 
 task('lq-tst', 'Excute Liquidation').setAction(async ({}, DRE) => {
   await DRE.run('set-DRE');
@@ -29,25 +34,38 @@ task('lq-tst', 'Excute Liquidation').setAction(async ({}, DRE) => {
   const liquidator = (await getEthersSigners())[1];
 
   const pool = LendingPoolFactory.connect('0x8022327a333eAeFaD46A723CDcA1aeFdA12afA53', liquidator);
+  const manager = LendingPoolCollateralManagerFactory.connect(
+    '0x6D4d18661D1776fEa8b40E885AB2Ed8919dd2CF2',
+    liquidator
+  );
   const usdc = '0x458db3bEf6ffC5212f9359bbDAeD0D5A58129397';
   const wsdn = '0x7cA69766F4be8Ec93dD01E1d571e64b867455e58';
   const weth = '0x72fE832eB0452285e91CA9F46B85229A5107CeE8';
+  const wbtc = '0xEdAA9f408ac11339766a4E5e0d4653BDee52fcA1';
+  const usdt = '0xEdAA9f408ac11339766a4E5e0d4653BDee52fcA1';
+  const wastr = '0x44a26AE046a01d99eBAbecc24B4d61B388656871';
   const lusdc = '0xF4D80e698D40Aae4F8486E59D3A52BB4b637e867';
   const target = '0x50414Ac6431279824df9968855181474c919a94B';
   const amountToLiquidate = parseEther('100000');
-  const lTokenInstance = await ERC20Factory.connect(lusdc, liquidator);
-  const amountBefore = await lTokenInstance.balanceOf(await liquidator.getAddress());
-  console.log('users config', await pool.getUserConfiguration(target));
-  console.log('users config', await pool.getUserConfiguration(await liquidator.getAddress()));
+  const { collateral, debt, lToken, liquidate } = {
+    collateral: usdc,
+    debt: weth,
+    lToken: lusdc,
+    liquidate: false,
+  };
+  const lTokenInstance = ERC20Factory.connect(lToken, liquidator);
+  console.log('health factor:', (await pool.getUserAccountData(target)).healthFactor.toString());
   console.log(
-    'health factor:',
-    await (await pool.getUserAccountData(target)).healthFactor.toString()
+    'ltoken before amount:',
+    await (await lTokenInstance.balanceOf(await liquidator.getAddress())).toString()
   );
-  console.log('ltoken before amount:', amountBefore.toString());
-  const tx = await pool
-    .connect(liquidator)
-    .liquidationCall(usdc, weth, target, amountToLiquidate, true);
-
+  await ERC20Factory.connect(debt, liquidator).approve(manager.address, parseEther('2000000'));
+  if (liquidate) {
+    const tx = await pool
+      .connect(liquidator)
+      .liquidationCall(collateral, debt, target, amountToLiquidate, true);
+    tx.wait();
+  }
   const amountAfter = await lTokenInstance.balanceOf(await liquidator.getAddress());
   console.log('ltoken after amount:', amountAfter.toString());
 });
