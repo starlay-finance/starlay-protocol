4406c9316016767b40d69d0bce0209c366848592
atatur9 <momodarou2015@gmail.com>
2022-03-03 17:24:14 +0900

    add uu

--- tasks/bots/liquidationCall.ts
+++ tasks/bots/liquidationCall.ts
@@ -12,18 +12,27 @@ task('liquidation-call', 'Liquidation call').setAction(async ({}, DRE) => {
   const signer = getEthersSigners();
   const network = <eNetwork>DRE.network.name;
   const pool = await getLendingPool('0x90384334333f3356eFDD5b20016350843b90f182');
-  const users = [...new Set(await getAllBorrowedUsers(pool))];
+  const borrowedTransactions = await getAllBorrowedUsers(pool);
+  const depositTransactions = await getAllDepositedUsers(pool);
+  console.log('borrowed', borrowedTransactions.length);
+  console.log('deposited', depositTransactions.length);
+  const uniqueBorrowedUsers = [...new Set(borrowedTransactions)];
+  const uniqueDepositedUsers = [...new Set(depositTransactions)];
+  console.log('unique deposited users', uniqueDepositedUsers.length);
+  console.log('unique borrowed users', uniqueDepositedUsers.length);
   console.log('Health Factors');
   const usersVSHealthFactors = {};
-  for (let i = 0; i < users.length; i++) {
-    const data = await pool.getUserAccountData(users[i]);
-    console.log(users[i], data.healthFactor.toString());
-    Object.keys(usersVSHealthFactors).push[users[i]] = data.healthFactor;
+  for (let i = 0; i < uniqueBorrowedUsers.length; i++) {
+    const data = await pool.getUserAccountData(uniqueBorrowedUsers[i]);
+    console.log(uniqueBorrowedUsers[i], data.healthFactor.toString());
+    Object.keys(usersVSHealthFactors).push[uniqueBorrowedUsers[i]] = data.healthFactor;
   }
   const liquidationTarget = Object.entries(usersVSHealthFactors).filter((e) => {
     const factor = e[1] as BigNumber;
     return factor.lt(parseEther('1'));
   });
+  console.log(await pool.getReservesList());
+
   console.log('liquidation targets', Object.keys(liquidationTarget));
 });
 
@@ -35,7 +44,7 @@ const getAllBorrowedUsers = async (pool: LendingPool) => {
   const current = await getCurrentBlock();
   while (from < current) {
     console.log('get event logs start from:', from);
-    const _got = await getLogs(provider, pool.address, from, from + blocksPerRequest);
+    const _got = await getBorrowed(provider, pool.address, from, from + blocksPerRequest);
     if (_got.length != 0) {
       _got.forEach((e) => {
         const log = pool.interface.parseLog(e);
@@ -47,11 +56,40 @@ const getAllBorrowedUsers = async (pool: LendingPool) => {
   return res;
 };
 
-const getLogs = async (provider: JsonRpcProvider, pool: string, from: number, to: number) => {
+const getAllDepositedUsers = async (pool: LendingPool) => {
+  const provider = new JsonRpcProvider('https://rpc.astar.network:8545');
+  const blocksPerRequest = 500;
+  let res: string[] = [];
+  let from = 508500;
+  const current = await getCurrentBlock();
+  while (from < current) {
+    console.log('get event logs start from:', from);
+    const _got = await getDeposited(provider, pool.address, from, from + blocksPerRequest);
+    if (_got.length != 0) {
+      _got.forEach((e) => {
+        const log = pool.interface.parseLog(e);
+        res.push(log.args[1]);
+      });
+    }
+    from += blocksPerRequest;
+  }
+  return res;
+};
+
+const getBorrowed = async (provider: JsonRpcProvider, pool: string, from: number, to: number) => {
   return await provider.getLogs({
     address: pool,
     fromBlock: from,
     toBlock: to,
     topics: [ethers.utils.id('Borrow(address,address,address,uint256,uint256,uint256,uint16)')],
+  });
+};
+
+const getDeposited = async (provider: JsonRpcProvider, pool: string, from: number, to: number) => {
+  return await provider.getLogs({
+    address: pool,
+    fromBlock: from,
+    toBlock: to,
+    topics: [ethers.utils.id('Deposit(address,address,address,uint256,uint16)')],
   });
 };
