7c127b3a97c5637470c8b52e65fefd69c2a6a385
un-ast <unast.eth@gmail.com>
2022-06-28 16:33:08 +0900

    update: use LTokenRev2 from the beginning

--- helpers/contracts-deployments.ts
+++ helpers/contracts-deployments.ts
@@ -616,7 +616,7 @@ export const deploySelfdestructTransferMock = async (verify?: boolean) =>
 export const chooseLTokenDeployment = (id: eContractid) => {
   switch (id) {
     case eContractid.LToken:
-      return deployGenericLTokenImpl;
+      return deployGenericLTokenRev2Impl;
     case eContractid.DelegationAwareLToken:
       return deployDelegationAwareLTokenImpl;
     default:
