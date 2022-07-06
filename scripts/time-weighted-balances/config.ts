a468c555b9f5ec539fa423a40a89aef24dba3c72
qudacki <qudacki@gmail.com>
2022-07-07 00:18:25 +0900

    update block number

--- scripts/time-weighted-balances/config.ts
+++ scripts/time-weighted-balances/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  fromBlockNumber: 1317550,
-  toBlockNumber: 1379387,
+  fromBlockNumber: 1321226,
+  toBlockNumber: 1380980,
   filterNullAddress: true,
   tokenAddress: '0x86eaded1f56ad656657b90d60483e1d0a5f7c20b',
   scriptDir: './scripts/time-weighted-balances',
