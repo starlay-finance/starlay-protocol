865e42a8e23ce2aff654d1a97fba9c73fedb56eb
atatur9 <momodarou2015@gmail.com>
2022-07-08 18:30:12 +0900

    change libs

--- tasks/deployments/deploy-lendingPoolImpls.ts
+++ tasks/deployments/deploy-lendingPoolImpls.ts
@@ -15,7 +15,10 @@ task(`deploy-LendingPoolImpls`, `Deploys LendingPoolImpls`)
     if (!localBRE.network.config.chainId) {
       throw new Error('INVALID_CHAIN_ID');
     }
-    const libs = await deployStarlayLibraries();
+    const libs = {
+      ['__$de8c0cf1a7d7c36c802af9a64fb9d86036$__']: '0xaCF53F9307A5B1616C7E35Fc0dEbd85e9925339A',
+      ['__$22cd43a9dda9ce44e9b92ba393b88fb9ac$__']: '0xa549A423BC2Aff1A566B644911C00c6Dc2F0390F',
+    };
     const refundable = await new RefundableLendingPoolFactory(libs, await getFirstSigner()).deploy(
       '0x5336C37c96B612749d019F86828885708f6BD10E'
     );
